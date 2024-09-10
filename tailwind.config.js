326e07433d6310351fb75a7ea4196541e063a395
anjali-ojha17 <anjaliojhazenith@gmail.com>
2024-09-10 22:41:11 +0530

    founders_grotesk

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "founders-grotesk": ["Founders Grotesk", "sans-serif"], // Custom font family
+      },
+    },
   },
   plugins: [],
 };
